469ae55f137b309f337088cac76c4d609cb9f3aa
gerball <gerball2193@gmail.com>
2024-05-09 15:09:32 +0100

    Adding txt file

--- Assignments/Assignment04-github.py
+++ Assignments/Assignment04-github.py
@@ -7,12 +7,16 @@ REPO_URL = 'git@github.com:Gerbs2193/WSAA---coursework.git'
 FILE_PATH = 'Assignments/Andrew.txt'
 YOUR_NAME = 'Ger'
 
+# Remove the temporary directory if it already exists
+if os.path.exists('/tmp/repo'):
+    shutil.rmtree('/tmp/repo')
+
 repo = Repo.clone_from(REPO_URL, '/tmp/repo')
 
 with open(os.path.join('/tmp/repo', FILE_PATH), 'r') as file:
     filedata = file.read()
 
-    filedata = filedata.replace('Andrew', YOUR_NAME)
+filedata = filedata.replace('Andrew', YOUR_NAME)
 
 with open(os.path.join('/tmp/repo', FILE_PATH), 'w') as file:
     file.write(filedata)
@@ -22,9 +26,9 @@ try:
     repo.git.commit('-m', 'Replace Andrew with ger')
     repo.git.push()
     print("Changes pushed successfully!")
-
 except Exception as e:
     print(f'An error has occurred: {e}')
 
 repo.close()
 shutil.rmtree('/tmp/repo')
+
